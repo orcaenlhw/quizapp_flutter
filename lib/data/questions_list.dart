d69396868ea16c423a22ed088eee8c23423b1844
edn <45616806+theednorc@users.noreply.github.com>
2022-03-01 20:59:31 +0630

    mit

---
+++ lib/data/questions_list.dart
@@ -0,0 +1,68 @@
+import 'package:numbercardgame/model/question_model.dart';
+
+//I just picked the list from the previous project
+// you can create your own list
+// I picked the question from national geo wild quiz for kids
+// but you can use your own list of question
+List<QuestionModel> questions = [
+  QuestionModel("Choose 1 ?", {
+    "2": false,
+    "7": false,
+    "1": true,
+    "0": false,
+  }),
+  QuestionModel("Choose 2 ?", {
+    "2": true,
+    "7": false,
+    "1": false,
+    "0": false,
+  }),
+  QuestionModel("Choose 3 ?", {
+    "2": false,
+    "7": false,
+    "1": false,
+    "3": true,
+  }),
+  QuestionModel("Choose 4 ?", {
+    "4": true,
+    "7": false,
+    "3": false,
+    "2": false,
+  }),
+  QuestionModel("Choose 5 ?", {
+    "7": false,
+    "6": false,
+    "2": false,
+    "5": true,
+  }),
+  QuestionModel("Choose 6 ?", {
+    "2": false,
+    "7": false,
+    "1": false,
+    "6": true,
+  }),
+  QuestionModel("Choose 7 ?", {
+    "9": false,
+    "2": false,
+    "3": false,
+    "7": true,
+  }),
+  QuestionModel("Choose 8 ?", {
+    "2": false,
+    "1": false,
+    "0": false,
+    "8": true,
+  }),
+  QuestionModel("Choose 9 ?", {
+    "3": false,
+    "5": false,
+    "6": false,
+    "9": true,
+  }),
+  QuestionModel("Choose 10 ?", {
+    "1": false,
+    "5": false,
+    "3": false,
+    "10": true,
+  })
+];
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:numbercardgame/data/questions_list.dart';
+import 'package:numbercardgame/screens/result_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,109 +10,179 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _HomePageState createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _HomePageState extends State<HomePage> {
+  //STEP 1: we need to create the folder structure of the app
+  //STEP 2: then we need to create the model for our Quiz Question
+  //STEP 3: and we need a simple list of question to display it
+  // in our screen, of course you can use your own list, or get data
+  // from and existing API
+  // Let's Start
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  //Let's store the colors code in a variable so it will be easy to use it
 
+  //Let's create a page controller for our pageview
+
+  Color mainColor = Color.fromARGB(255, 9, 17, 77);
+  Color secondColor = Color(0xFF117eeb);
+
+  PageController? _controller = PageController(initialPage: 0);
+
+  // Setting the game variable
+  bool isPressed = false;
+  Color isTrue = Colors.green;
+  Color isWrong = Colors.red;
+  Color btnColor = Color(0xFF117eeb);
+  int score = 0;
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      backgroundColor: mainColor,
+      body: Padding(
+        padding: EdgeInsets.all(18.0),
+        child: PageView.builder(
+          // now we need to disable the scrolling so we can navigate only with the button
+          physics: NeverScrollableScrollPhysics(),
+          controller: _controller!,
+          onPageChanged: (page) {
+            setState(() {
+              isPressed = false;
+            });
+          },
+          itemCount: questions.length,
+          itemBuilder: (context, index) {
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(
+                  width: double.infinity,
+                  child: Text(
+                    "Question ${index + 1} / ${questions.length}",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w300,
+                      fontSize: 28.0,
+                    ),
+                  ),
+                ),
+                const Divider(
+                  color: Colors.white,
+                  height: 8.0,
+                  thickness: 1.0,
+                ),
+                const SizedBox(
+                  height: 20.0,
+                ),
+                Text(
+                  questions[index].question!,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 28.0,
+                  ),
+                ),
+                SizedBox(
+                  height: 35.0,
+                ),
+                // Okay now we need to generate a list of buttons, that will represent the answer to the questions
+                // for that we will use a for loop
+                for (int i = 0; i < questions[index].answer!.length; i++)
+                  Container(
+                    width: double.infinity,
+                    margin: EdgeInsets.only(bottom: 18.0),
+                    child: MaterialButton(
+                      shape: StadiumBorder(),
+                      color: isPressed
+                          ? questions[index].answer!.entries.toList()[i].value
+                              ? isTrue
+                              : isWrong
+                          : secondColor,
+                      padding: EdgeInsets.symmetric(vertical: 18.0),
+                      onPressed: isPressed
+                          ? () {}
+                          : () {
+                              setState(() {
+                                isPressed = true;
+                              });
+                              if (questions[index]
+                                  .answer!
+                                  .entries
+                                  .toList()[i]
+                                  .value) {
+                                score += 10;
+                                print(score);
+                              }
+                            },
+                      child: Text(
+                        questions[index].answer!.keys.toList()[i],
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                SizedBox(
+                  height: 50.0,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    OutlinedButton(
+                      onPressed: isPressed
+                          ? index + 1 == questions.length
+                              ? () {
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) =>
+                                              ResultScreen(score)));
+                                }
+                              : () {
+                                  _controller!.nextPage(
+                                      duration: Duration(milliseconds: 500),
+                                      curve: Curves.linear);
+                                  setState(() {
+                                    isPressed = false;
+                                  });
+                                }
+                          : null,
+                      style: OutlinedButton.styleFrom(
+                        shape: StadiumBorder(),
+                        side: BorderSide(color: Colors.orange, width: 1.0),
+                      ),
+                      // we will style the button later
+                      //now let's start creating the game mechanics
+
+                      child: Text(
+                          index + 1 == questions.length
+                              ? "See result"
+                              : "Next Question",
+                          style: TextStyle(
+                            color: Colors.white,
+                          )),
+                    ),
+                  ],
+                ),
+              ],
+            );
+          },
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
